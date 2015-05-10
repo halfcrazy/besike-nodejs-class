2910c27cf8afbd0d083f4ad3da79aa552bf9f4e2
halfcrazy <hackzhuyan@gmail.com>
2015-05-10 15:13:51 +0800

    still failure

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-exports.Class=function (argument) {
+function Class(properties, parent) {
+  var cons = properties.initialize ? properties.initialize : new Function;
+  if (parent) {
+    cons.prototype = new parent();
+    cons.prototype.constractor = cons;
+    cons.__super__ = parent;
+  } else {
+    cons.__super__ = Object;
+  }
+
+  Object.keys(properties).forEach(function(prop) {
+    if (prop !== 'initialize') {
+      cons.prototype[prop] = properties[prop];
+    }
+  });
+
+  cons.prototype.super = function(name) {
+    return parent.prototype[name].apply(this, [].slice.call(arguments, 1));
+  }
+  return cons;
 };
+
+module.exports = Class;
